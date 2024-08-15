bb253f977c6f0843815c4dfdaab5fa4eedfd4e7d
limhaekyu <limk9500@gmail.com>
2024-08-15 15:48:50 +0900

    Initial commit

---
+++ src/main/java/com/cookyuu/ecms_server/EcmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.cookyuu.ecms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmsServerApplication.class, args);
+	}
+
+}
