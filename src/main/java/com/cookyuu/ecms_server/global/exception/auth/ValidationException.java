7b1c00c93fe713966baa7f28e0aa68cea3d5d482
cookyuu <limk9500@gmail.com>
2024-09-12 01:12:43 +0900

    feat(log): logback 설정 추가

--- src/main/java/com/cookyuu/ecms_server/global/exception/auth/ValidationException.java
+++ src/main/java/com/cookyuu/ecms_server/global/exception/auth/ValidationException.java
@@ -3,11 +3,11 @@ package com.cookyuu.ecms_server.global.exception.auth;
 import com.cookyuu.ecms_server.global.dto.ResultCode;
 import com.cookyuu.ecms_server.global.exception.ECMSAppException;
 
-public class ValidatePhoneNumberException extends ECMSAppException {
-    public ValidatePhoneNumberException() {
+public class ValidateFormatException extends ECMSAppException {
+    public ValidateFormatException() {
         super(ResultCode.INVALID_INPUT_VALUE);
     }
-    public ValidatePhoneNumberException(ResultCode resultCode) {
+    public ValidateFormatException(ResultCode resultCode) {
         super(resultCode);
     }
 }
