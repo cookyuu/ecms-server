3ac51b27e8a7d436c93034d1490ea5e117e13c0c
cookyuu <limk9500@gmail.com>
2024-09-15 00:36:02 +0900

    feat(product): 상품 정보 수정 기능 개발

---
+++ src/main/java/com/cookyuu/ecms_server/global/exception/domain/ECMSProductException.java
@@ -0,0 +1,31 @@
+package com.cookyuu.ecms_server.global.exception.domain;
+
+import com.cookyuu.ecms_server.global.dto.ResultCode;
+import com.cookyuu.ecms_server.global.exception.ECMSAppException;
+
+public class ECMSProductException extends ECMSAppException {
+
+    public ECMSProductException() {
+        super(ResultCode.PRODUCT_NOT_FOUND);
+    }
+
+    public ECMSProductException(ResultCode resultCode) {
+        super(resultCode);
+    }
+
+    protected ECMSProductException(ResultCode resultCode, String customMsg) {
+        super(resultCode, customMsg);
+    }
+
+    protected ECMSProductException(ResultCode resultCode, Object data, String[] args) {
+        super(resultCode, data, args);
+    }
+
+    protected ECMSProductException(ResultCode resultCode, Throwable t) {
+        super(resultCode, t);
+    }
+
+    protected ECMSProductException(ResultCode resultCode, Throwable t, String customMsg) {
+        super(resultCode, t, customMsg);
+    }
+}
