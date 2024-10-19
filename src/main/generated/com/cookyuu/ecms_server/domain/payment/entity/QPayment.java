b6723f1925a227de9f94815d4f2773271df2c66a
cookyuu <limk9500@gmail.com>
2024-10-19 17:16:43 +0900

    add(QEntity): QueryDSL을 위한 QEntity 생성

---
+++ src/main/generated/com/cookyuu/ecms_server/domain/payment/entity/QPayment.java
@@ -0,0 +1,61 @@
+package com.cookyuu.ecms_server.domain.payment.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPayment is a Querydsl query type for Payment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPayment extends EntityPathBase<Payment> {
+
+    private static final long serialVersionUID = -948967661L;
+
+    public static final QPayment payment = new QPayment("payment");
+
+    public final com.cookyuu.ecms_server.global.entity.QBaseTimeEntity _super = new com.cookyuu.ecms_server.global.entity.QBaseTimeEntity(this);
+
+    public final NumberPath<Integer> amount = createNumber("amount", Integer.class);
+
+    public final DateTimePath<java.time.LocalDateTime> canceledAt = createDateTime("canceledAt", java.time.LocalDateTime.class);
+
+    public final StringPath cancelReason = createString("cancelReason");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath orderNumber = createString("orderNumber");
+
+    public final StringPath paymentFailMsg = createString("paymentFailMsg");
+
+    public final EnumPath<PaymentMethod> paymentMethod = createEnum("paymentMethod", PaymentMethod.class);
+
+    public final StringPath paymentNumber = createString("paymentNumber");
+
+    public final EnumPath<PaymentStatus> status = createEnum("status", PaymentStatus.class);
+
+    public QPayment(String variable) {
+        super(Payment.class, forVariable(variable));
+    }
+
+    public QPayment(Path<? extends Payment> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPayment(PathMetadata metadata) {
+        super(Payment.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/cookyuu/ecms_server/domain/product/entity/QProduct.java
@@ -0,0 +1,80 @@
+package com.cookyuu.ecms_server.domain.product.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = 1149999091L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final com.cookyuu.ecms_server.global.entity.QBaseTimeEntity _super = new com.cookyuu.ecms_server.global.entity.QBaseTimeEntity(this);
+
+    public final ListPath<com.cookyuu.ecms_server.domain.cart.entity.CartItem, com.cookyuu.ecms_server.domain.cart.entity.QCartItem> cartItems = this.<com.cookyuu.ecms_server.domain.cart.entity.CartItem, com.cookyuu.ecms_server.domain.cart.entity.QCartItem>createList("cartItems", com.cookyuu.ecms_server.domain.cart.entity.CartItem.class, com.cookyuu.ecms_server.domain.cart.entity.QCartItem.class, PathInits.DIRECT2);
+
+    public final QCategory category;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = createDateTime("deletedAt", java.time.LocalDateTime.class);
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isDeleted = createBoolean("isDeleted");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath name = createString("name");
+
+    public final ListPath<com.cookyuu.ecms_server.domain.order.entity.OrderLine, com.cookyuu.ecms_server.domain.order.entity.QOrderLine> orderLines = this.<com.cookyuu.ecms_server.domain.order.entity.OrderLine, com.cookyuu.ecms_server.domain.order.entity.QOrderLine>createList("orderLines", com.cookyuu.ecms_server.domain.order.entity.OrderLine.class, com.cookyuu.ecms_server.domain.order.entity.QOrderLine.class, PathInits.DIRECT2);
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final ListPath<com.cookyuu.ecms_server.domain.review.entity.Review, com.cookyuu.ecms_server.domain.review.entity.QReview> reviews = this.<com.cookyuu.ecms_server.domain.review.entity.Review, com.cookyuu.ecms_server.domain.review.entity.QReview>createList("reviews", com.cookyuu.ecms_server.domain.review.entity.Review.class, com.cookyuu.ecms_server.domain.review.entity.QReview.class, PathInits.DIRECT2);
+
+    public final com.cookyuu.ecms_server.domain.seller.entity.QSeller seller;
+
+    public final NumberPath<Integer> stockQuantity = createNumber("stockQuantity", Integer.class);
+
+    public QProduct(String variable) {
+        this(Product.class, forVariable(variable), INITS);
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QProduct(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QProduct(PathMetadata metadata, PathInits inits) {
+        this(Product.class, metadata, inits);
+    }
+
+    public QProduct(Class<? extends Product> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new QCategory(forProperty("category"), inits.get("category")) : null;
+        this.seller = inits.isInitialized("seller") ? new com.cookyuu.ecms_server.domain.seller.entity.QSeller(forProperty("seller")) : null;
+    }
+
+}
+
