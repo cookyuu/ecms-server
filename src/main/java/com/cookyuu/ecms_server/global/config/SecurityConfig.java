dbd58113450572f25df0ae805176cf4e61bfc792
cookyuu <limk9500@gmail.com>
2024-10-21 03:42:04 +0900

    feature(swagger): Swagger API 명세서 추가

--- src/main/java/com/cookyuu/ecms_server/global/config/SecurityConfig.java
+++ src/main/java/com/cookyuu/ecms_server/global/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     private static final String[] AUTH_WHITELIST = {
-        "/api/v1/auth/**", "/api/v1/seller/registration", "api/v1/order/search"
+        "/api/v1/auth/**", "/api/v1/seller/registration", "/api/v1/order/search", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
     };
     private static final String[] AUTH_ADMIN = {
     };
