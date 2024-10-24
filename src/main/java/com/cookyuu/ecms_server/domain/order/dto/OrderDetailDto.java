0cfc9cd099aa8421ef5ab606ca3dba1ad3449a45
cookyuu <limk9500@gmail.com>
2024-10-25 02:32:47 +0900

    feature(get-detail): 주문 상세 내용 조회 기능 개발

--- src/main/java/com/cookyuu/ecms_server/domain/order/dto/OrderDetailDto.java
+++ src/main/java/com/cookyuu/ecms_server/domain/order/dto/OrderDetailDto.java
@@ -1,4 +1,36 @@
 package com.cookyuu.ecms_server.domain.order.dto;
 
+import com.cookyuu.ecms_server.domain.order.entity.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDetailDto {
+    private String orderNumber;
+    private Integer totalPrice;
+    private OrderStatus status;
+    private String destination;
+    private String destinationDetail;
+    private String recipientName;
+    private String recipientPhoneNumber;
+    private Long buyerId;
+    private String buyerLoginId;
+    private String shipmentNumber;
+    private List<OrderLineInfo> orderLines;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OrderLineInfo {
+        private Long productId;
+        private String productName;
+        private Integer quantity;
+        private Integer price;
+        private Long sellerId;
+    }
 }
--- src/main/java/com/cookyuu/ecms_server/domain/order/repository/OrderCustomRepository.java
+++ src/main/java/com/cookyuu/ecms_server/domain/order/repository/OrderCustomRepository.java
@@ -1,8 +1,10 @@
 package com.cookyuu.ecms_server.domain.order.repository;
 
+import com.cookyuu.ecms_server.domain.order.dto.OrderDetailDto;
 import com.cookyuu.ecms_server.domain.order.dto.SearchOrderDto;
 import org.springframework.data.domain.Page;
 
 public interface OrderCustomRepository {
     Page<SearchOrderDto.Response> searchPageOrderByCreatedAtDesc(SearchOrderDto.Request request);
+    OrderDetailDto getOrderDetail(String orderNumber);
 }
