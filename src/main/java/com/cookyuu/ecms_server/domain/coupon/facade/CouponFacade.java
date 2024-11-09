d21a617014ee22fa3e89dc215a7c79d4f771b032
임해규 <limk9500@gmail.com>
2024-11-09 10:59:22 +0900

    test(coupon): 쿠폰 발급 테스트 작성

--- src/main/java/com/cookyuu/ecms_server/domain/coupon/facade/CouponFacade.java
+++ src/main/java/com/cookyuu/ecms_server/domain/coupon/facade/CouponFacade.java
@@ -2,13 +2,15 @@ package com.cookyuu.ecms_server.domain.coupon.facade;
 
 import com.cookyuu.ecms_server.domain.coupon.dto.IssueCouponDto;
 import com.cookyuu.ecms_server.domain.coupon.entity.Coupon;
-import com.cookyuu.ecms_server.domain.coupon.repository.CouponRepository;
 import com.cookyuu.ecms_server.domain.coupon.service.CouponService;
 import com.cookyuu.ecms_server.domain.coupon.service.IssueCouponService;
 import com.cookyuu.ecms_server.domain.member.entity.Member;
 import com.cookyuu.ecms_server.domain.member.service.MemberService;
+import com.cookyuu.ecms_server.global.code.ResultCode;
+import com.cookyuu.ecms_server.global.exception.domain.ECMSCouponException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +20,14 @@ public class CouponFacade {
     private final MemberService memberService;
     private final IssueCouponService issueCouponService;
 
-    @Transactional
     public void issueCoupon(long memberId, IssueCouponDto.Request couponInfo) {
-
         Coupon coupon = couponService.issueCoupon(couponInfo);
-        Member member = memberService.findMemberById(memberId);
-        issueCouponService.issueCoupon(member, coupon);
+        try {
+            Member member = memberService.findMemberById(memberId);
+            issueCouponService.issueCoupon(member, coupon);
+        } catch (Exception e) {
+            couponService.issueCouponFail(couponInfo);
+            throw new ECMSCouponException(ResultCode.BAD_REQUEST, "쿠폰 발급 실패");
+        }
     }
 }
